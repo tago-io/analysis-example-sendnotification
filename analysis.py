ffcc99be513dd058a5a0586ba8e6be93df0f628d
Vitor Lima <vitorfdl@tago.io>
2019-10-01 10:25:03 -0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,21 +1,41 @@
 # Analysis Example
-# Hello World
+# Send Notification to Yourself
 
-# Learn how to send messages to the console located on the TagoIO analysis screen.
-# You can use this principle to show any information during and after development.
+# The main function used by Tago to run the script.
+# It sends a notification to the account and another one linked to a dashboard.
+
+# Environment Variables
+# You must setup the following Environment Variables:
+# message - Your Message
+# title - Your Title
+# ref_id - A bucket ID or a Dashboard ID.
+
 
 from tago import Analysis
+from tago import Services
 
 # The function myAnalysis will run when you execute your analysis
 def myAnalysis(context, scope):
-  # This will log "Hello World" at the TagoIO Analysis console
-  context.log("Hello World")
+  message = list(filter(lambda message: message['key'] == 'message', context.environment))
+  message = message[0]['value']
 
-  #  This will log the environment to the TagoIO Analysis console
-  context.log('Environment:', context.environment)
+  title = list(filter(lambda title: title['key'] == 'title', context.environment))
+  title = title[0]['value']
 
-  #  This will log the scope to the TagoIO Analysis console
-  context.log('my scope:', scope)
+
+  ref_id = list(filter(lambda ref_id: ref_id['key'] == 'dashboard;_id', context.environment))
+  if not ref_id:
+    ref_id = list(filter(lambda ref_id: ref_id['key'] == 'bucket_id', context.environment))
+    if not ref_id:
+      ref_id = None
+    else:
+      ref_id = ref_id[0]['value']
+  else:
+    ref_id = ref_id[0]['value']
+
+  notification = Services(context.token).notification
+  notification.send(title, message, ref_id)
+
 
 # The analysis token in only necessary to run the analysis outside TagoIO
 Analysis('MY-ANALYSIS-TOKEN-HERE').init(myAnalysis)
